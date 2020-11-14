23759b569266f7fe73ae74364dbb9329916de65f
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-11-14 19:58:00 +0530

    UI Fixed

--- IPT/app/src/main/java/com/example/ipt/LoginActivity.java
+++ IPT/app/src/main/java/com/example/ipt/LoginActivity.java
@@ -2,13 +2,44 @@ package com.example.ipt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button regBtn,loginBtn;
+        TextView forgotPw;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        regBtn = findViewById(R.id.nav_reg);
+        forgotPw = findViewById(R.id.forgot_pw);
+        loginBtn = findViewById(R.id.btn_login);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SignupActivity.class));
+            }
+        });
+
+        forgotPw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ForgotPasswordActivity.class));
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), StudentDashboardActivity.class));
+            }
+        });
     }
 }
--- IPT/app/src/main/java/com/example/ipt/StudentDashboardActivity.java
+++ IPT/app/src/main/java/com/example/ipt/StudentDashboardActivity.java
@@ -2,13 +2,35 @@ package com.example.ipt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class StudentDashboardActivity extends AppCompatActivity {
+
+    TextView addQualification, profileSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_dashboard);
+
+        addQualification = findViewById(R.id.add_qualification);
+        profileSetting = findViewById(R.id.profile_settings);
+
+        addQualification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AddQualificationActivity.class));
+            }
+        });
+
+        profileSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), UpdateProfileActivity.class));
+            }
+        });
     }
 }
